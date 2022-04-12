3b9a0ba855308efbcd3ff81a3afb01bb1e5e8a51
Björn Larsson <bjorn.larsson@2xper.se>
2022-04-12 20:02:21 +0200

    Compressing solution.

--- UkrainianFlag/Program.cs
+++ UkrainianFlag/Program.cs
@@ -7,26 +7,7 @@ namespace UkrainianFlag
         //See https://codegolf.stackexchange.com/questions/245318/draw-the-ukrainian-flag for more information.
         static void Main(string[] args)
         {
-            var blue = Color.FromArgb(0, 87, 183);
-            var yellow = Color.FromArgb(255, 215, 0);
-
-            var flag = new Bitmap(78, 52);
-            for (int y = 0; y < flag.Height; y++)
-            {
-                for (int x = 0; x < flag.Width; x++)
-                {
-                    if (y >= flag.Height / 2)
-                    {
-                        flag.SetPixel(x, y, blue);
-                    }
-                    else
-                    {
-                        flag.SetPixel(x, y, yellow);
-                    }
-                }
-            }
-
-            flag.Save("flag.bmp");
+            var f=new Bitmap(78,52);for(int y=0;y<52;y++)for(int x=0;x<78;x++)if(y>25)f.SetPixel(x,y,Color.FromArgb(255,215,0));else f.SetPixel(x,y,Color.FromArgb(0,87,183));f.Save("f.bmp");
         }
     }
 }
