ec73bf7566e482b14e2fbdcea5838e73ad849671
Björn Larsson <bjorn.larsson@2xper.se>
2022-04-12 18:38:17 +0200

    Creating empty project.

---
+++ UkrainianFlag/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UkrainianFlag
+{
+    internal class Program
+    {
+        //See https://codegolf.stackexchange.com/questions/245318/draw-the-ukrainian-flag for more information.
+        static void Main(string[] args)
+        {
+        }
+    }
+}
